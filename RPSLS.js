3f8879546eb77dbabe242ee01ec3b57567954a61
brooke <73143900+oyasumimizi@users.noreply.github.com>
2020-10-28 17:55:24 -0500

    Add files via upload

---
+++ RPSLS.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock.");
+console.log("You will have the choice to play with an AI or a second player. Please make a choice.");
+console.log("Alright. Now, how the game goes is that one of the players (AI or second player will need to win 3 rounds to win the game.");
+console.log("The way this works is, rock crushes scissors, scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, and Spock vaporizes rock.");
+console.log("If you understand how this works, press the begin button to start the game in the console. Let the game begin!");
